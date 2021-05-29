52999afcd825d17a6b4e49551706363dddedd7e9
bo-yakitarako <bo-yakitarako@users.noreply.github.com>
2021-05-29 15:27:58 +0900

    styled-component導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
+import { StylesProvider } from '@material-ui/styles';
 
-const App: React.FC = () => <div>うんちぶり</div>;
+const App: React.FC = () => (
+  <StylesProvider injectFirst>
+    <AppWrapper>うんちぶり</AppWrapper>
+  </StylesProvider>
+);
 
 export { App };
+
+const AppWrapper = styled.div`
+  position: fixed;
+  display: flex;
+  width: 100%;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  justify-content: center;
+  align-items: center;
+`;
