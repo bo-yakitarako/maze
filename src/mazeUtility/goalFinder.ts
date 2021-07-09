6a3cf5896d3ba9c8ae8495b13d366b1c673bedac
bo-yakitarako <bo-yakitarako@users.noreply.github.com>
2021-07-10 00:13:20 +0900

    もっとリファクタリングできたねー

--- src/mazeUtility/goalFinder.ts
+++ src/mazeUtility/goalFinder.ts
@@ -8,30 +8,21 @@ const findAnswer = (mazeArray: boolean[][]) => {
     [1, mazeArray.length - 1],
     [1, mazeArray.length - 2],
   ] as Point[];
-  return _findAnswer(mazeArray, foundRoads, [...foundRoads]);
+  return _findAnswer(mazeArray, foundRoads);
 };
 
 // eslint-disable-next-line no-underscore-dangle, @typescript-eslint/naming-convention
-const _findAnswer = (maze: boolean[][], roads: Point[], longest: Point[]) => {
+const _findAnswer = (maze: boolean[][], roads: Point[]) => {
   let foundRoads = [...roads];
   let nextPoints = getNextPoints(maze, foundRoads);
   while (nextPoints.length === 1) {
     foundRoads = [...foundRoads, nextPoints[0]];
-    if (nextPoints[0][1] === 0) {
-      return { reach: foundRoads, longest };
-    }
     nextPoints = getNextPoints(maze, foundRoads);
   }
-  if (nextPoints.length === 0 && foundRoads.length > longest.length) {
-    longest = foundRoads; // eslint-disable-line no-param-reassign
-  }
-  const defaultAnswer = { reach: [...foundRoads], longest };
+  const defaultAnswer = { reach: foundRoads, longest: foundRoads };
   return nextPoints.reduce((pre, nextPoint) => {
     const afterBranch = [...foundRoads, nextPoint];
-    if (nextPoint[1] === 0) {
-      return { ...pre, reach: afterBranch };
-    }
-    const deadEnd = _findAnswer(maze, afterBranch, pre.longest) as Road;
+    const deadEnd = _findAnswer(maze, afterBranch) as Road;
     const [, reachLastY] = deadEnd.reach[deadEnd.reach.length - 1];
     const reach = reachLastY === 0 ? deadEnd.reach : pre.reach;
     if (pre.longest.length < deadEnd.longest.length) {
@@ -44,7 +35,7 @@ const _findAnswer = (maze: boolean[][], roads: Point[], longest: Point[]) => {
 const getNextPoints = (mazeArray: boolean[][], foundRoads: Point[]) => {
   const [[currentX, currentY], [preX, preY]] = [...foundRoads].reverse();
   return DIRECTIONS.map(([x, y]) => [currentX + x, currentY + y]).filter(
-    ([x, y]) => mazeArray[y][x] && !(x === preX && y === preY),
+    ([x, y]) => y >= 0 && mazeArray[y][x] && !(x === preX && y === preY),
   ) as Point[];
 };
 
